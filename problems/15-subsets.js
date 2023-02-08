9158df12f7ab9933f2d76a8f2729f63d111d846c
Prabhjot Singh <pjsingh1359@gmail.com>
2023-02-08 11:20:12 -0800

    finish 15

--- problems/15-subsets.js
+++ problems/15-subsets.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 Write a function called `subsets` that will return all subsets of an array.
 
-Examples: 
+Examples:
 
 subsets([]) // [[]]
 subsets([1]) // [[], [1]]
@@ -13,10 +13,36 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
   2. For every subset that does not contain 3, there is also a corresponding
      subset that is the same, except it also does contain 3.
 ***********************************************************************/
+/*
+ - an array that contains subsets of arrays that have unique pairs
+ - input: array
+ - output: an array with sub arrays;
 
-const subsets = (arr) => {
-  // Your code here
-}
+ plan:
+  - initialize an array with a empty sub array to start
+  - slice off the 3? store it away and make pairs with 1 and 2?
+  -
+*/
+// const subsets = (arr) => {
+//   // Your code here
+
+//   if(arr.length === 0) {
+//     return [[]];
+//   }
+//   if (arr.length === 1) {
+//     return [[], arr];
+//   }
+
+
+// }
+
+const subsets = ([x, ...xs], ys = []) =>
+  x == undefined
+    ? [ys]
+  : [...subsets (xs, ys), ...subsets (xs, [...ys, x])]
+
+console.log(subsets([])) // [[]]
+console.log(subsets([1])) // [[], [1]]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
